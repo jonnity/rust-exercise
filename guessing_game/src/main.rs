e1c1087bd7f26ade8fe081f74e76fd498a65ab22
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-27 14:01:05 +0000

    make guessing in loop and break if player win

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,21 +6,28 @@ fn main() {
     println!("Guess the number!"); // 数を当ててごらん
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
+    loop {
+        println!("Please input your guess."); // ほら、予想を入力してね
 
-    println!("Please input your guess."); // ほら、予想を入力してね
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); //数値を入力してください！
-
-    println!("You guessed: {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),  //小さすぎ！
-        Ordering::Greater => println!("Too big!"), //大きすぎ！
-        Ordering::Equal => println!("You win!"),   //やったね！
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  //小さすぎ！
+            Ordering::Greater => println!("Too big!"), //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } //やったね！
+        }
     }
 }
