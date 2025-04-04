4dca7de0ea6965733836a9408dfc81a245f56121
jonnity <jonny1996_ty@yahoo.co.jp>
2025-04-04 10:08:39 +0000

    change the renamed method: `rand::thread_rng().gen_range()` -> `rand::random_range(1..101)`

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,9 @@
-use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!"); // 数を当ててごらん
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::random_range(1..101);
     // println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
     loop {
         println!("Please input your guess."); // ほら、予想を入力してね
