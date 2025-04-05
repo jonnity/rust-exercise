3b69be24aaf9f1977bd847e01c5fa808854eef92
jonnity <jonny1996_ty@yahoo.co.jp>
2025-04-05 14:48:14 +0000

    3.1節の内容を試す

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+const PI_THREE_DIGIT = 3.14
+// let PI_THREE_DIGIT = 3.14
+// グローバルスコープでは↑とは書けない
+
+fn main() {
+    // mutにしておけば、束縛している値を変更可能
+    let mut x1 = 5;
+    println!("The value of x1 is: {}", x1); // xの値は{}です
+    x1 = 6;
+    println!("The value of x1 is: {}", x1);
+
+    // mutじゃなくても、shadowingによって、値を変更することは可能
+    // ここでは単に{}でブロックを形成しているけど、別関数に渡していると考えると、有用そう
+    // ブロック外に副作用がないことを保証できてるから
+    let x2 = 5;
+    let x2 = x2 + 1;
+    {
+        let x2 = x2 * 2;
+        println!("The value of x2 in the inner scope is: {}", x2);
+    }
+    println!("The value of x2 is: {}", x2);
+}
