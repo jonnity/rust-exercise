ed68c24b0e5a96b3c798af5079d9a9d06a60a5b2
jonnity <jonny1996_ty@yahoo.co.jp>
2025-04-13 14:42:11 +0000

    add unit tests to Guess

--- guessing_game/src/guess.rs
+++ guessing_game/src/guess.rs
@@ -14,3 +14,28 @@ impl Guess {
         Ok(Guess { value })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Guess;
+
+    #[test]
+    fn valid_guess() {
+        let guess = Guess::new(50);
+        assert!(guess.is_ok());
+        assert_eq!(guess.unwrap().value, 50);
+        let guess = Guess::new(1);
+        assert!(guess.is_ok());
+        assert_eq!(guess.unwrap().value, 1);
+        let guess = Guess::new(100);
+        assert!(guess.is_ok());
+        assert_eq!(guess.unwrap().value, 100);
+    }
+    #[test]
+    fn invalid_guess() {
+        let guess = Guess::new(101);
+        assert!(guess.is_err());
+        let guess = Guess::new(0);
+        assert!(guess.is_err());
+    }
+}
