2842a7c6f939d1583bc2da09305b22a0da30959d
jonnity <jonny1996_ty@yahoo.co.jp>
2025-04-17 14:06:28 +0000

    implement minigep just to read the specified file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,25 @@
+extern crate minigrep;
+
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    // {}を探しています
-    println!("Searching for {}", query);
-    // {}というファイルの中
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
